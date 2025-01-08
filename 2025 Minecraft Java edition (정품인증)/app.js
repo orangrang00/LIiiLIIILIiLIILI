d0339ba7bca426e470fa12485fb71c0276587305
orangrang00 <oranbe1234@naver.com>
2025-01-08 12:14:29 +0900

    Add files via upload

---
+++ 2025 Minecraft Java edition (정품인증)/app.js
@@ -0,0 +1,13 @@
+ // app.js
+ const colorButton = document.querySelector("#color-button");
+
+ colorButton.addEventListener("click", () => {
+     document.body.style.backgroundColor = "#FF5733";
+ });
+
+ // app.js
+ const color2Button = document.querySelector("#color2-button");
+
+ color2Button.addEventListener("click", () => {
+     document.body.style.backgroundColor = "#FF6978";
+ }); 
